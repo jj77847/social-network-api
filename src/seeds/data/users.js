c272c5b40f0c6e2c1e066dc3d4d499ab2aa014e1
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-15 14:01:09 +0000

    users

---
+++ src/seeds/data/users.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    username: "jonathanjefferies",
+    email: "jonathanjefferies@gmail.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "kimberleyjefferies",
+    email: "kimberleyjefferies@gmail.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "ellenjefferies",
+    email: "ellenjefferies@gmail.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "hannahjefferies",
+    email: "hannahjefferies@gmail.com",
+    thoughts: [],
+    friends: [],
+  },
+  {
+    username: "spikejefferies",
+    email: "spikejefferies@gmail.com",
+    thoughts: [],
+    friends: [],
+  },
+];
+
+module.exports = users;
